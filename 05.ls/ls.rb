90ee8ccaa987873d55a7b2d5870c1efd639ab61a
KOH-0712 <kohichi.s0712@gmail.com>
2023-10-04 16:50:52 +0900

    クラスからメソッドに変更

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,33 +3,26 @@
 
 MAX_COLUMN = 3
 
-class LsCommand1
-  def initialize
-    @files = Dir.glob('*')
-    max_letter = @files.max_by(&:length).length
-    @ljust_files = @files.map { |x| x.ljust(max_letter) }
-  end
+def decide_lines(files)
+  number_of_files = files.length
+  number_of_files.ceildiv(MAX_COLUMN)
+end
 
-  def decide_lines
-    number_of_files = @files.length
-    number_of_files.ceildiv(MAX_COLUMN)
-  end
+def arrange_files(files, line)
+  ljust_files = files.map { |x| x.ljust(files.map(&:length).max) }
+  sliced_files = ljust_files.each_slice(line).to_a
+  sliced_files.each { |a| a[line - 1] = nil if a.length < line }
+  sliced_files.transpose
+end
 
-  def arrange_files(line)
-    sliced_files = @ljust_files.each_slice(line).to_a
-    sliced_files.each { |a| a[line - 1] = nil if a.length < line }
-    sliced_files.transpose
-  end
-
-  def output_files(transposed_files)
-    fixed_files = (transposed_files.map { |x| x.join(' ') })
-    puts fixed_files
-  end
+def output_files(transposed_files)
+  fixed_files = (transposed_files.map { |x| x.join(' ') })
+  puts fixed_files
 end
 
 if __FILE__ == $PROGRAM_NAME
-  result = LsCommand1.new
-  line = result.decide_lines
-  transposed_files = result.arrange_files(line)
-  result.output_files(transposed_files)
+  files = Dir.glob('*')
+  line = decide_lines(files)
+  transposed_files = arrange_files(files, line)
+  output_files(transposed_files)
 end
